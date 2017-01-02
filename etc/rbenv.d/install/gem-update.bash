16f2a404f4842c4196bc353c58d4baf134e0a712
n0ts <naoya.n@gmail.com>
2017-01-02 16:44:23 +0900

    Only support CRuby 2.4.0 or later

--- etc/rbenv.d/install/gem-update.bash
+++ etc/rbenv.d/install/gem-update.bash
@@ -4,10 +4,18 @@ else
   echo "rbenv: rbenv-gem-update plugin requires ruby-build 20130129 or later" >&2
 fi
 
+verlte() {
+  [ "$1" = "$2" ] && return 1 || [  "$2" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
+
 gem_update_system() {
   # Exit if Ruby wasn't installed successfully
   [ "$STATUS" = "0" ] || return 0
 
+  # Only CRuby
+  [[ "$VERSION_NAME" != 2.* ]] && return 0
+
+  # CRuby version 2.4.0 or later
   # Invoke `gem update --system` in the just-installed Ruby.
-  RBENV_VERSION="$VERSION_NAME" rbenv-exec gem update --system
+  verlte $VERSION_NAME 2.4 && RBENV_VERSION="$VERSION_NAME" rbenv-exec gem update --system
 }
